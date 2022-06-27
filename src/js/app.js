7d0551896184d99ae36fbeba29667d6e64a08797
tghostGosha <tghost@mail.ru>
2022-06-27 17:41:24 +0300

    done account form

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,8 @@ import * as flsFunctions from "./modules/functions.js";
 import $ from "jquery"
 import { Inputmask } from "inputmask";
 import Swiper, { Navigation, Pagination } from 'swiper';
-// import Inputmask from "inputmask.es6.js";
-// import 'bootstrap';
+import JustValidate from 'just-validate';
+
 Swiper.use([Navigation, Pagination])
 
 flsFunctions.isWebp()
@@ -17,39 +17,42 @@ const heroSwiper = new Swiper('.hero-swiper', {
   slidesPerGroup: 1,
   spaceBetween: 50,
   loop: true,
-  
+
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
 })
 
-// let selector = document.querySelectorAll('input[type="tel"]');
-
-// let im = new Inputmask("+7 (999) 999-99-99");
-// im.mask(selector);
-
 //====делаем первую букву  в Верхнем регистре
 const regex = /[A-Za-z0-0]/;
-const inputs = document.getElementsByClassName('form-control')
-
-for (let i = 0; i < inputs.length; ++i) {
-  inputs[i].onblur = () => {
-    // if (regex.test(inputs[i].value)) inputs[i].value='';
-    if(inputs[i].value === '') return;
+let firstLetterToUpperCase = (className) => {
+  const inputs = document.getElementsByClassName(className)
   
-    let str = inputs[i].value
-    .trim()
-    .replace(/-+/g, '-')
-    .replace(/^-|-$/g, '')
-    .replace(/\s+/g, ' ')
-    .trim()
-    inputs[i].value = str[0].toUpperCase() + str.substr(1).toLowerCase()
-  }
+  
+    for (let i = 0; i < inputs.length; ++i) {
+      inputs[i].onblur = () => {
+        // if (regex.test(inputs[i].value)) inputs[i].value='';
+        if (inputs[i].value === '') return;
+  
+        let str = inputs[i].value
+          .trim()
+          .replace(/-+/g, '-')
+          .replace(/^-|-$/g, '')
+          .replace(/\s+/g, ' ')
+          .trim()
+        inputs[i].value = str[0].toUpperCase() + str.substr(1).toLowerCase()
+      }
+    }
+  
 }
+firstLetterToUpperCase('form-control')
+
+
+
 //=====Input mask
 const mask = event => {
-  const {target, keyCode, type} = event;
+  const { target, keyCode, type } = event;
 
   const pos = target.selectionStart;
   if (pos < 3) event.preventDefault();
@@ -84,3 +87,111 @@ if (input.type === 'tel') {
 }
 
 
+
+
+
+//========Валидация формы открытия счета и маска================
+      //======маска телефон
+const accountFormTel = document.getElementById('tel');
+if (accountFormTel.type === 'tel') {
+  accountFormTel.addEventListener('input', mask);
+  accountFormTel.addEventListener('focus', mask);
+  accountFormTel.addEventListener('blur', mask);
+  accountFormTel.addEventListener('keydown', mask);
+}
+      //======заглавная буква
+firstLetterToUpperCase('account__form-input');
+      //=======Валидация
+const validation = new JustValidate('#accountform', {
+  errorFieldCssClass: 'is-invalid',
+  errorLabelStyle: {
+    fontSize: '14px',
+    color: '#dc3545',
+  },
+  successFieldCssClass: 'is-valid',
+  successLabelStyle: {
+    fontSize: '14px',
+    color: '#20b418',
+
+  },
+  successFieldStyle: {
+    border: '1px solid #44953D',
+  },
+  focusInvalidField: true,
+  lockForm: true,
+});
+validation
+  .addField('#firstname', [
+    {
+      rule: 'minLength',
+      value: 3,
+      errorMessage: 'Фамилия должна содержать не менее 3-х символов ',
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле',
+    },
+  ])
+  .addField('#secondname', [
+    {
+      rule: 'minLength',
+      value: 2,
+      errorMessage: 'Имя должно содержать не менее 2-х символов ',
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле',
+    },
+  ])
+  .addField('#surname', [
+    {
+      rule: 'minLength',
+      value: 3,
+      errorMessage: 'Отчество должно содержать не менее 3-х символов ',
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле',
+    },
+  ])
+  .addField('#mail', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Не валидный Email',
+    },
+  ])
+  .addField('#tel', [
+    {
+      rule: 'required',
+      errorMessage: 'Обязательное поле',
+    },
+  ])
+  .addField('#accountCheck', [
+    {
+      rule: 'required',
+      errorMessage: 'Подтвердите',
+    },
+  ])
+  // {
+  //   successMessage: 'Силён! с первой попытки'
+  // }
+  .onSuccess((ev) => {
+    ev.preventDefault();
+    window.showNotification();
+  });
