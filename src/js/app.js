fed28249add89413361a9666dc3c746e97e0c744
tghostGosha <tghost@mail.ru>
2022-07-07 16:00:36 +0300

    individual done

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
-import $ from "jquery"
+import $, { type } from "jquery"
 import { Inputmask } from "inputmask";
 import Swiper, { Navigation, Pagination } from 'swiper';
 import JustValidate from 'just-validate';
 import noUiSlider from 'nouislider';
-
+import wNumb from 'wnumb';
 
 
 Swiper.use([Navigation, Pagination])
@@ -233,49 +233,265 @@ validation
 // getStocks()
 
 const slider1 = document.getElementById('slider1');
+const slider2 = document.getElementById('slider2');
 
 noUiSlider.create(slider1, {
-  start: [10000],
+  start: [0],
   connect: [true, false],
   range: {
-      'min': 10000,
-      'max': 400000
+    'min': 10000,
+    'max': 400000
   },
   step: 10000,
+  format: wNumb({
+    decimals: 0,
+    thousand: ' '
+  }),
 });
-function crossUpdate(value, slider) {
+noUiSlider.create(slider2, {
+  start: [0],
+  connect: [true, false],
+  range: {
+    'min': 3,
+    'max': 10
+  },
+  step: 1,
+  format: wNumb({
+    decimals: 0
+  }),
+});
 
-  // If the sliders aren't interlocked, don't
-  // cross-update.
-  if (!lockedState) return;
+let slider1Value = document.getElementById('slider1Value');
+let slider2Value = document.getElementById('slider2Value');
+let resultYears = document.getElementById('resultYears');
+let resultTotalVnesenie = document.getElementById('resultTotalVnesenie');
+let count
+let years
 
-  // Select whether to increase or decrease
-  // the other slider value.
-  var a = slider1 === slider ? 0 : 1;
+slider2.noUiSlider.on('update', function (values, handle) {
+  years = slider2.noUiSlider.get(true)
+  if (years <= 4) {
+    slider2Value.innerHTML = `${years} года`;
+    resultYears.textContent = `Мои сбережения за ${years} года`
+    
+  } else {
+    slider2Value.innerHTML = `${years} лет`;
+    resultYears.textContent = `Мои сбережения за ${years} лет`
+    
+  }
+  console.log(years)
+  return years
+});
 
-  // Invert a
-  var b = a ? 0 : 1;
-
-  // Offset the slider value.
-  value -= lockedValues[b] - lockedValues[a];
-
-  // Set the value
-  slider.noUiSlider.set(value);
-}
-
-var slider1Value = document.getElementById('slider1Value');
-
-function setLockedValues() {
-  lockedValues = [
-      Number(slider1.noUiSlider.get()),
-     
-  ];
-}
-
-slider1.noUiSlider.on('change', setLockedValues);
 slider1.noUiSlider.on('update', function (values, handle) {
-  slider1Value.innerHTML = values[handle];
+  count = Math.round(slider1.noUiSlider.get(true))
+  slider1Value.innerHTML = `${count}₽`;
+  console.log(count)
+  
+  return count
 });
-slider1.noUiSlider.on('slide', function (values, handle) {
-  crossUpdate(values[handle], slider2);
-});
+
+console.log(count)
+console.log(years)
+
+// resultTotalVnesenie = new Number(0)
+// resultTotalVnesenie.textContent = Math.round(valueSlider1 * valueSlider2);
+
+// console.log(resultTotalVnesenie)
+
+// $(document).ready(function() {
+//   // Optimalisation: Store the references outside the event handler:
+//   let $window = $(window);
+//   let $pane = $('#pane1');
+
+//   function checkWidth() {
+//     let windowsize = $window.width();
+//     if (windowsize < 800) {
+//       $('.advantage .b-section-info__text').css('background','none');
+//       //$('.advantage .b-section-info__text:nth-child').css('background','none');
+//     }
+//     else{
+//       $('.advantage .b-section-info__text').eq(0).css('background','url(../img/arrow-left.png) no-repeat top center');
+//       $('.advantage .b-section-info__text').eq(1).css('background','url(../img/arrow-right.png) no-repeat top center');
+//     }
+//   }
+//   // Execute on load
+//   checkWidth();
+//   // Bind event listener
+//   $(window).resize(checkWidth);
+
+
+//   $(".b-pricing-info__item.f-pricing-info__item").click(function(){
+//     $(".b-pricing-info__item.f-pricing-info__item").removeClass("active");
+//     $(this).addClass("active");
+
+//     let newTitle = $(this).parent().parent().find(".title").find("h2").text();
+//     console.log ('newTitle '+newTitle);
+
+//     let newPercent = $(this).attr("percent");
+//     let newPercentText = $(this).attr("percentText");
+
+//     let newCapitalize = $(this).attr("capitalize");
+
+//     console.log ('newCapitalize '+newCapitalize);
+
+//     if(newCapitalize!=1){
+//       $("ins.capitalize").addClass("hide");
+//       $(".ndfl").addClass("hide");
+//     } else {
+//       $("ins.capitalize").removeClass("hide");
+//       $(".ndfl").removeClass("hide");
+//     }
+
+//     $("#percent").attr("val",newPercent);
+//     $("#percentText").html(newPercentText);
+//     $("#name").text(newTitle)
+//     $("span.name").text(newTitle)
+//     $("span.name").attr("name",newTitle);
+//     $(".form form textarea").text("Меня интересует ИИС "+newTitle+". Прошу связаться со мной...")
+//     calc(newCapitalize);
+//   })
+// });
+
+// function scrollToForm(){
+// window.scrollTo(0, $("#formScroll").offset().top-50);	
+// }
+
+// function calc() {
+
+//   // $(".SliderUnits.s400").html("<i></i><i></i><i></i><i></i>");
+//   // $(".SliderUnits.sYears").html("<i></i><i></i><i></i><i></i><i></i><i></i><i></i>");
+
+//   // var capitalize = new Number($(".f-pricing-info__item.active").attr("capitalize"))
+
+//   let result = new Number(0);
+
+//   var dohodDU = new Number(0);
+//   var totalDohodDU = new Number(0);
+
+//   var start = new Number($("#start").val());
+
+//   var years = new Number($("#years").val());
+
+//   if (start <= 10001) {
+//     start = 10000;
+//     $("#start").val("10000");
+//   };
+
+//   if (start >= 400001) {
+//     start = 400000;
+//     $("#start").val("400000");
+//   };
+
+//   if (years <= 3.1) {
+//     years = 3;
+//     $("#years").val("3");
+//   };
+
+//   if (years >= 10) {
+//     years = 10;
+//     $("#years").val("10");
+//   };
+
+//   let vichet = new Number($("#vichet").attr("val"));
+//   let percent = new Number($("#percent").attr("val"));
+
+
+//   percent = new Number(percent / 100 + 1);
+//   let percent2 = (percent * 10000 - 10000) / 10000;
+//   vichet = vichet / 100;
+
+//   let vichetTotal = Math.round(vichet * start * years);
+
+
+//   //var thisPercent = new Number(resultPercent/100+1);
+//   let prevResult = new Number(0);
+
+//   // 0 = Брокерский, 1 = Накопительный
+//   //if(capitalize==0){
+//   //var result = Math.round((start*(vichet+percent))*years);
+//   for (let i = 1; i < years + 1; i++) {
+
+//     let prev = new Number(i - 1);
+//     if (prev < 0) { prev = 0 };
+
+//     dohodDU = (start + prevResult) * percent2;
+//     totalDohodDU = totalDohodDU + dohodDU;
+
+//     result = Math.round((prevResult + start) + dohodDU);
+//     prevResult = result;
+
+//     console.log('start ' + start + ',prevResult ' + prevResult + ',percent2 ' + percent2 + ',result ' + result);
+
+//   }
+
+//   // if (capitalize == 1) {
+//   //   ndfl = Math.round((totalDohodDU - 0.001 * (start * years + dohodDU)) * 0.13);
+//   //   //ndfl =  (result-(start*years)-vichetTotal)*0.13
+//   //   result = result - ndfl;
+//   // }
+
+
+//   result = Math.round(result + vichetTotal);
+//   let resultPercentMoney = Math.round(result - vichetTotal - (start * years));
+
+
+//   percent = Math.round(resultPercentMoney - vichet)
+
+//   //var resultPercent = Math.round(((result/(resultTotalVnesenie/2))-1)*1000/years)/10;
+
+//   let param = result / resultTotalVnesenie;
+//   param = param.toFixed(4);
+//   console.log(param);
+//   param = (param - 1).toFixed(4);
+//   let resultPercent = param * 100;
+//   resultPercent = resultPercent.toFixed(2);
+
+//   resultTotalVnesenie = addSpaces(resultTotalVnesenie);
+//   //resultPercent = addSpaces(resultPercent);
+//   ndfl = addSpaces(ndfl);
+//   vichetTotal = addSpaces(vichetTotal);
+//   percent = addSpaces(percent);
+//   result = addSpaces(result);
+
+//   $("#vichet").html(vichetTotal + '<i class="fa fa-rub"></i>');
+//   $("#ndfl").html(ndfl + '<i class="fa fa-rub"></i>');
+//   $("#percent").html(percent + '<i class="fa fa-rub"></i>');
+//   $("#result").html(result + '<i class="fa fa-rub"></i>');
+//   $("#resultTotalVnesenie").html(resultTotalVnesenie + '<i class="fa fa-rub"></i>');
+//   $("#resultPercent").html(resultPercent + '%');
+//   $("#resultYears").html(years);
+
+
+// }
+// if(capitalize==1){
+//   for (var i = 0; i < years; i++) {
+//      result = Math.round((result+start)*percent);
+//      //alert(start*vichet)
+//   }
+//   result =  Math.round(result+vichetTotal);
+// }
+
+// меняем 3 года, 5 лет
+// if(years > 4){
+//   $("ins.years").text("лет");
+// }
+// if(years < 5){
+//   $("ins.years").text("года");
+// }
+
+
+
+
+
+function addSpaces(nStr) {
+  nStr += '';
+  let x = nStr.split('.');
+  let x1 = x[0];
+  let x2 = x.length > 1 ? '.' + x[1] : '';
+  let rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ' ' + '$2');
+  }
+  return x1 + x2;
+}
